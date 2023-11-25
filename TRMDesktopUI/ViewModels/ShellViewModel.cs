e8af2a8870e358ab178ea04bfc591c7f5d44299a
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-11-25 15:24:30 +0800

    Displaying Users and Groups/Roles

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -39,6 +39,11 @@ namespace TRMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void UserManagement()
+        {
+            ActivateItem(IoC.Get<UserDisplayViewModel>());
+        }
+
         public void LogOut()
         {
             _user.ResetUserModel();
