805e9e415d2f54c5ad2c5ae27eec0f07de78ef0c
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-12-08 11:23:48 +0800

    Solve Tokens Not Working in Swagger (fix AB#22)

--- TRMApi/Program.cs
+++ TRMApi/Program.cs
@@ -60,6 +60,29 @@ namespace TRMApi
                         Title = "TimCO Retail Manager API",
                         Version = "v1"
                     });
+                setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
 
             var app = builder.Build();
